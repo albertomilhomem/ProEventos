38abf7d8c5bfc5d1cf417c707f43ab2ad733fc29
ALberto Milhomem <alberto.milho@gmail.com>
2022-11-22 16:24:25 -0400

    Capitulo 2 - .NET Core 5 e 6

---
+++ Back/src/ProEventos.Api/Controllers/EventoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> evento = new Evento[]{
+            new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Porto Velho",
+                    Lote = "Primeiro Lote",
+                    QuantidadePessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+                },
+                new Evento()
+                {
+                    EventoId = 2,
+                    Tema = "VueJS e Laravel",
+                    Local = "São Paulo",
+                    Lote = "Primeiro Lote",
+                    QuantidadePessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+                }
+                };
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return evento;
+        }        
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+            return evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
