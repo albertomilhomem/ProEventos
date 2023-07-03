9253c56b4400161851ae9992c4e95e38ec89d7b4
Alberto Milhomem <alberto.milho@gmail.com>
2023-07-03 18:07:33 -0400

    Finalizado a Aula 127, 128, 129, 130 e 131

---
+++ Back/src/ProEventos.Application/DTO/EventoDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.DTO
+{
+    public class EventoDTO
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QuantidadePessoas { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
