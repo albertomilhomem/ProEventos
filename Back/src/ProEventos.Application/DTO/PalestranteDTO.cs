cb9f2220d06943cc945f2ba37f2b79a96b086e34
Alberto Milhomem <alberto.milho@gmail.com>
2023-07-04 10:35:38 -0400

    Correcão arquivo PalestranteDTO

--- Back/src/ProEventos.Application/DTO/PalestranteDTO.cs
+++ Back/src/ProEventos.Application/DTO/PalestranteDTO.cs
@@ -7,6 +7,14 @@ namespace ProEventos.Application.DTO
 {
     public class PalestranteDTO
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<RedeSocialDTO> RedesSociais { get; set; }
+        public IEnumerable<PalestranteDTO> Palestrantes { get; set; }
+
     }
 }
