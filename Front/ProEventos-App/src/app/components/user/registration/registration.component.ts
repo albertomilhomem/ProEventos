b4b8d430c42c2e4bcc309cf606f667041216792c
Alberto Milhomem <alberto.milho@gmail.com>
2023-08-01 22:50:17 -0400

    Aula 232

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ValidatorField } from '@app/helpers/ValidatorField';
+import { User } from '@app/models/identity/User';
+import { AccountService } from '@app/services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -8,38 +12,44 @@ import { ValidatorField } from '@app/helpers/ValidatorField';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
+  user = {} as User;
   form!: FormGroup;
 
   get f(): any {
     return this.form.controls;
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router, private toaster: ToastrService) { }
 
   ngOnInit(): void {
     this.validation();
   }
 
-  public validation(): void 
-  {
+  public validation(): void {
     const formOptions: AbstractControlOptions = {
-      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+      validators: ValidatorField.MustMatch('password', 'confirmePassword')
     }
 
 
     this.form = this.fb.group(
       {
-        nome: ['', Validators.required],
-        sobrenome: ['', Validators.required],
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
         username: ['', Validators.required],
-        senha: ['', [Validators.required, Validators.minLength(6)]],
-        confirmeSenha: ['', Validators.required]
+        password: ['', [Validators.required, Validators.minLength(4)]],
+        confirmePassword: ['', Validators.required]
       },
-
       formOptions
     )
-   }
+  }
+
+  register(): void {
+    this.user = { ... this.form.value };
+    this.accountService.register(this.user).subscribe(
+      () => this.router.navigateByUrl('/dashboard'),
+      (error: any) => this.toaster.error(error.error)
+    )
+  }
 
 }
